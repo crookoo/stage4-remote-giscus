eba717e3c5710e6eb4c7153ea5d94015f6999487
Michael Zimmermann <info@michaelzimmermann.com>
2023-02-06 09:46:19 +0100

    Removed ID display

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,26 @@ export default function App({ postId }: Props) {
     const params = new URLSearchParams(window.location.search);
     const urlId = params.get('id');
     const id = postId ? postId : (urlId ? urlId : '0');
+    console.log('remote app:', id);
 
     return (
-        <>
-            <div>{id}</div>
-            <div className="comments-container">
-                <Giscus
-                    id="comments"
-                    repo="crookoo/stage4-comments"
-                    repoId="R_kgDOIncVKw"
-                    category="Announcements"
-                    categoryId="DIC_kwDOIncVK84CTEy4"
-                    mapping="specific"
-                    term={id}
-                    reactionsEnabled="1"
-                    emitMetadata="0"
-                    inputPosition="top"
-                    theme={theme}
-                    lang="de"
-                    loading="eager"
-                    strict="1"
-                />
-            </div>
-        </>
+        <div className="comments-container">
+            <Giscus
+                id="comments"
+                repo="crookoo/stage4-comments"
+                repoId="R_kgDOIncVKw"
+                category="Announcements"
+                categoryId="DIC_kwDOIncVK84CTEy4"
+                mapping="specific"
+                term={id}
+                reactionsEnabled="1"
+                emitMetadata="0"
+                inputPosition="top"
+                theme={theme}
+                lang="de"
+                loading="eager"
+                strict="1"
+            />
+        </div>
     );
 }
